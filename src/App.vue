c310352d1f022bfc116678fe0e915a5e45addbcc
acer-mono <margaritastar92@gmail.com>
2021-09-11 13:42:39 +0700

    create searchBlock

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <SearchBox :options="options" :select-handler="selectHandler" />
+  <div>Some information for test</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import SearchBox from "@/components/SearchBox.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    SearchBox,
+  },
+  data() {
+    return {
+      options: ["one", "two", "three"],
+      selectHandler(value: string): void {
+        console.log(value);
+      },
+    };
   },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="main">
+    <div class="search-panel">
+      <input
+        placeholder="Type city here"
+        @focus="isInputInFocus = true"
+        @blur="isInputInFocus = false"
+        v-model="searchString"
+      />
+      <img src="../assets/binoculars.svg" alt="binoculars" width="50" />
+    </div>
+    <div class="options" v-if="isInputInFocus && searchString">
+      <div
+        class="option"
+        v-for="(value, key) in getSuitableOptions"
+        :key="key"
+        @mousedown="selectHandler(value)"
+      >
+        {{ value }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SearchBox",
+  props: {
+    options: {
+      type: Array as () => Array<string>,
+      required: true,
+    },
+    selectHandler: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      searchString: "",
+      isInputInFocus: false,
+    };
+  },
+  computed: {
+    getSuitableOptions(): Array<string> {
+      return this.options.filter((el) =>
+        el.toLowerCase().includes(this.searchString.toLowerCase())
+      );
+    },
+  },
+});
+</script>
+
+<style scoped>
+.main {
+  position: relative;
+}
+.search-panel {
+  display: flex;
+  flex-flow: row wrap;
+  background: #bcbcbc;
+  padding: 1rem 2rem;
+}
+input {
+  all: unset;
+  font-size: 1.5rem;
+  color: #707070;
+  background: #ffffff;
+  padding: 0.5rem;
+  flex-grow: 1;
+  margin-right: 1.75rem;
+}
+.options {
+  position: absolute;
+  left: 0;
+  right: 0;
+  font-size: 1.5rem;
+  color: #707070;
+  border: 1px solid #e8e8e8;
+  border-top: 0;
+}
+.option {
+  padding: 1rem 2rem;
+  background: #ffffff;
+  border-top: 1px solid #e8e8e8;
+}
+
+.option:first-child {
+  border: 0;
+}
+
+.option:hover {
+  color: #ffffff;
+  background: #c6c6c6;
+}
+</style>
