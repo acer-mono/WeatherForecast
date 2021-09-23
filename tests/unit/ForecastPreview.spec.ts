c3cd180e689beeca9cac230de8cedd5e7fa836ee
Margarita Ivanova <Margarita Ivanova>
2021-09-23 10:28:29 +0700

    some tests

---
+++ tests/unit/ForecastPreview.spec.ts
@@ -0,0 +1,25 @@
+import { shallowMount } from "@vue/test-utils";
+import ForecastPreview from "@/components/ForecastPreview.vue";
+
+describe("ForecastPreview.vue", () => {
+  const forecast = {
+    id: "1",
+    date: "22/02/2020",
+    temperature: 10.1,
+    humidity: 11.5,
+    windSpeed: 15,
+    windDirection: 12,
+    airPressure: 11,
+    visibility: 0,
+    icon: "none",
+  };
+  const wrapper = shallowMount(ForecastPreview, {
+    props: { forecast },
+  });
+
+  it("render elements", async () => {
+    expect(wrapper.text()).toMatch(forecast.date);
+    expect(wrapper.text()).toMatch(forecast.temperature.toString());
+    expect(wrapper.text()).toMatch(forecast.humidity.toString());
+  });
+});
--- tests/unit/SearchBox.spec.ts
+++ tests/unit/SearchBox.spec.ts
@@ -2,13 +2,31 @@ import { shallowMount } from "@vue/test-utils";
 import SearchBox from "@/components/SearchBox.vue";
 
 describe("SearchBox.vue", () => {
-  it("renders props.options when passed", async () => {
-    const options = ["first", "second"];
-    const wrapper = shallowMount(SearchBox, {
-      props: { options },
-    });
-    const input = wrapper.find("input");
+  const options = ["first", "second"];
+  const wrapper = shallowMount(SearchBox, {
+    props: { options },
+  });
+  const input = wrapper.find("input");
+
+  it("search elements", async () => {
     await input.setValue(options[0][0]);
+    await input.trigger("focus");
+    expect(wrapper.vm.searchString).toEqual(options[0][0]);
+    expect(wrapper.vm.isInputInFocus).toEqual(true);
     expect(wrapper.text()).toMatch(options[0]);
+  });
+
+  it("blur after searching elements", async () => {
+    await input.setValue(options[0][0]);
+    await input.trigger("focus");
+    await input.trigger("blur");
+    expect(wrapper.text()).toBe("");
+  });
+
+  it("clear search", async () => {
+    await input.setValue(options[0][0]);
+    await input.trigger("focus");
+    await input.setValue("");
+    expect(wrapper.text()).toBe("");
   });
 });
