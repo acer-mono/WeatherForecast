96c5f5af907f6bec3243ddd2906d387421623ddc
Margarita Ivanova <Margarita Ivanova>
2021-09-30 10:23:59 +0700

    add tests

--- tests/unit/SearchBox.spec.ts
+++ tests/unit/SearchBox.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import SearchBox from "@/components/SearchBox.vue";
+import Vuex from "vuex";
 
 describe("SearchBox.vue", () => {
   const options = ["first", "second"];
@@ -14,6 +15,30 @@ describe("SearchBox.vue", () => {
     expect(wrapper.vm.searchString).toEqual(options[0][0]);
     expect(wrapper.vm.isInputInFocus).toEqual(true);
     expect(wrapper.text()).toMatch(options[0]);
+  });
+
+  it("select item", async () => {
+    const actions = {
+      getCurrentForecast: jest.fn(),
+      getFiveDaysForecasts: jest.fn(),
+    };
+    const store = new Vuex.Store({ actions });
+    const wrapper = shallowMount(SearchBox, {
+      props: { options },
+      global: {
+        mocks: {
+          $store: store,
+        },
+      },
+    });
+    const input = wrapper.find("input");
+    await input.setValue(options[0][0]);
+    await input.trigger("focus");
+    const option = wrapper.find(".option");
+
+    await option.trigger("mousedown");
+    expect(actions.getFiveDaysForecasts).toHaveBeenCalled();
+    expect(actions.getCurrentForecast).toHaveBeenCalled();
   });
 
   it("blur after searching elements", async () => {
